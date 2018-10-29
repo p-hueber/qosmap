6d73af197b0b77dd8e6ed9733b7876a2e37cfbf4
Paul Hüber <phueber@kernsp.in>
2018-10-29 10:42:35 +0100

    Move the control connection stuff into its own module
    The structures used between client and server to initiate the probing
    deserve their own module 'control'.

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ extern crate serde_json;
 extern crate structopt;
 
 mod analyze;
+mod control;
 mod flow;
 
 use analyze::sequence::{ReSequencer, SequenceReport, Sequencer};
 use flow::Flow;
+use control::{ControlMessage, ControlStream};
 use std::env;
-use std::io::{Read, Write};
 use std::net::SocketAddr;
 use std::net::ToSocketAddrs;
 use std::net::{TcpListener, TcpStream, UdpSocket};
@@ -40,54 +41,6 @@ struct Opt {
     /// duration of the test in seconds
     #[structopt(short = "d", long = "duration", default_value = "1")]
     duration: u64,
-}
-
-trait ControlStream {
-    fn send_msg(&mut self, ControlMessage) -> Result<(), String>;
-    fn recv_msg(&mut self) -> Result<ControlMessage, String>;
-}
-
-impl<T> ControlStream for T
-where
-    T: Read + Write,
-{
-    fn send_msg(&mut self, msg: ControlMessage) -> Result<(), String> {
-        let mut data = serde_json::to_vec(&msg).map_err(|e| e.to_string())?;
-        data.push(0);
-        self.write(&data)
-            .and(self.flush())
-            .map_err(|e| e.to_string())
-            .map(|_| ())
-    }
-
-    fn recv_msg(&mut self) -> Result<ControlMessage, String> {
-        use std::io::{BufRead, BufReader};
-        let mut buf_stream = BufReader::new(self);
-        let mut message_data: Vec<u8> = Vec::new();
-
-        let bytes = buf_stream
-            .read_until(0, &mut message_data)
-            .map_err(|e| e.to_string())?;
-
-        if bytes == 0 || message_data[bytes - 1] != 0 {
-            // short read due to EOF
-            return Err("Control connection closed by remote side".to_string());
-        } else {
-            message_data.pop();
-            let message: ControlMessage = serde_json::from_slice(
-                &message_data,
-            ).map_err(|e| e.to_string())?;
-            Ok(message)
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum ControlMessage {
-    RequestFlow,
-    ExpectFlow(u16),
-    TerminateFlow(u16),
-    Report(SequenceReport),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
